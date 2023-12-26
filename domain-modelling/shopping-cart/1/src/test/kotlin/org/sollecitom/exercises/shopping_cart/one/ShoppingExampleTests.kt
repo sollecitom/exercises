5f7b1d8dff84ccbfb86997d0610577b0e2ec1259
Michele Sollecito <michele.sollecito@gmail.com>
2023-12-26 20:19:58 +0100

    WIP

--- domain-modelling/shopping-cart/1/src/test/kotlin/org/sollecitom/exercises/shopping_cart/one/ShoppingExampleTests.kt
+++ domain-modelling/shopping-cart/1/src/test/kotlin/org/sollecitom/exercises/shopping_cart/one/ShoppingExampleTests.kt
@@ -1,6 +1,7 @@
 package org.sollecitom.exercises.shopping_cart.one
 
 import assertk.assertThat
+import assertk.assertions.isEqualTo
 import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
@@ -8,19 +9,26 @@ import org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS
 import org.sollecitom.chassis.core.domain.currency.Currency
 import org.sollecitom.chassis.core.domain.currency.SpecificCurrencyAmount
 import org.sollecitom.chassis.core.domain.currency.known.Dollars
+import org.sollecitom.chassis.core.domain.currency.known.cents
 import org.sollecitom.chassis.core.domain.currency.known.currency
 import org.sollecitom.chassis.core.domain.currency.known.toCurrencyAmount
+import org.sollecitom.chassis.core.domain.currency.times
+import org.sollecitom.chassis.core.domain.identity.Id
+import org.sollecitom.chassis.core.domain.naming.Name
+import org.sollecitom.chassis.core.domain.traits.Identifiable
 import org.sollecitom.chassis.core.test.utils.isZero
+import org.sollecitom.chassis.core.test.utils.testProvider
+import org.sollecitom.chassis.core.utils.CoreDataGenerator
 import java.math.BigDecimal
 
 @TestInstance(PER_CLASS)
-private class ShoppingExampleTests {
+private class ShoppingExampleTests : CoreDataGenerator by CoreDataGenerator.testProvider {
 
     // TODO Tests
     // 1 banana
     // 2 bananas
     // 2 apples and 3 bananas
-    // an unknown product
+    // an unsupported product
     // 3x2 discount
     // different prices on different days
     // a beer when the shopper is 14
@@ -40,9 +48,48 @@ private class ShoppingExampleTests {
         assertThat(bill.total).isZero()
     }
 
-    private inline fun <reified CURRENCY : SpecificCurrencyAmount<CURRENCY>> newShop(): Shop<CURRENCY> = InMemoryShop(CURRENCY::class.currency)
+    @Test
+    fun `paying for some products`() = runTest {
+
+        val shopper = newShopper()
+        val banana = newProduct("Banana")
+        val bananaPrice = 60.cents
+        val shop = newShop<Dollars>(prices = mapOf(banana to bananaPrice))
+
+        shopper.addToCart(banana)
+        val bill = with(shop) {
+            shopper.checkout()
+        }
+
+        assertThat(bill.total).isEqualTo(bananaPrice)
+    }
+
+    private fun newProduct(name: String, id: Id = newId.forEntities()): Product = ProductReference(id, name.let(::Name))
+
+    private inline fun <reified CURRENCY : SpecificCurrencyAmount<CURRENCY>> newShop(prices: Map<Product, CURRENCY> = emptyMap()): Shop<CURRENCY> = InMemoryShop(CURRENCY::class.currency, prices)
 
     private fun newShopper(): Shopper = InMemoryShopper()
+}
+
+class ProductReference(override val id: Id, override val name: Name) : Product {
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is ProductReference) return false
+
+        if (id != other.id) return false
+
+        return true
+    }
+
+    override fun hashCode() = id.hashCode()
+
+    override fun toString() = "ProductReference(id=$id, name=$name)"
+}
+
+interface Product : Identifiable {
+
+    val name: Name
 }
 
 internal class InMemoryShopper : Shopper {
@@ -54,17 +101,35 @@ internal class InMemoryShopper : Shopper {
 
         return this@Shop.billFor(cart)
     }
+
+    override suspend fun addToCart(product: Product) {
+
+        cart.add(product)
+    }
 }
 
 data class InMemoryBill<CURRENCY : SpecificCurrencyAmount<CURRENCY>>(override val total: CURRENCY) : Bill<CURRENCY>
 
-internal class InMemoryShop<CURRENCY : SpecificCurrencyAmount<CURRENCY>>(private val currency: Currency<CURRENCY>) : Shop<CURRENCY> {
+internal class InMemoryShop<CURRENCY : SpecificCurrencyAmount<CURRENCY>>(private val currency: Currency<CURRENCY>, private val prices: Map<Product, CURRENCY>) : Shop<CURRENCY> {
+
+    private val zero = BigDecimal.ZERO.toCurrencyAmount(currency)
 
     override suspend fun billFor(cart: ShoppingCart): Bill<CURRENCY> {
 
-        val total = BigDecimal.ZERO.toCurrencyAmount(currency)
+        val total = cart.items.map { it.asCartItem() }.map(::cost).fold(zero, SpecificCurrencyAmount<CURRENCY>::plus)
         return InMemoryBill(total)
     }
+
+    private fun cost(item: CartItem): CURRENCY {
+
+        val (product, quantity) = item
+        val price = prices[product] ?: error("Unsupported product $product")
+        return price * quantity
+    }
+
+    private data class CartItem(val product: Product, val quantity: Int)
+
+    private fun Map.Entry<Product, Int>.asCartItem(): CartItem = CartItem(key, value)
 }
 
 interface Shop<CURRENCY : SpecificCurrencyAmount<CURRENCY>> {
@@ -74,16 +139,26 @@ interface Shop<CURRENCY : SpecificCurrencyAmount<CURRENCY>> {
 
 interface ShoppingCart {
 
+    val items: Map<Product, Int>
 }
 
 internal class InMemoryShoppingCart : ShoppingCart {
 
+    private val _items = mutableMapOf<Product, Int>()
+    override val items: Map<Product, Int> get() = _items
+
+    fun add(product: Product) {
+
+        _items.compute(product) { _, quantity -> (quantity ?: 0) + 1 }
+    }
 }
 
 interface Shopper {
 
     context(Shop<CURRENCY>)
     suspend fun <CURRENCY : SpecificCurrencyAmount<CURRENCY>> checkout(): Bill<CURRENCY>
+
+    suspend fun addToCart(product: Product)
 }
 
 interface Bill<CURRENCY : SpecificCurrencyAmount<CURRENCY>> {
